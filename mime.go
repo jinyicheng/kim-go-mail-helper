11910e84e0376a3a2adc9be3797410f7a9ef0a96
jinyicheng <jinyicheng@live.com>
2024-07-30 13:03:08 +0800

    init

---
+++ mime.go
@@ -0,0 +1,19 @@
+package mailHelper
+
+import (
+	"mime"
+	"mime/quotedprintable"
+	"strings"
+)
+
+var newQPWriter = quotedprintable.NewWriter
+
+type mimeEncoder struct {
+	mime.WordEncoder
+}
+
+var (
+	bEncoding     = mimeEncoder{mime.BEncoding}
+	qEncoding     = mimeEncoder{mime.QEncoding}
+	lastIndexByte = strings.LastIndexByte
+)
